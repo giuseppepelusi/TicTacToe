3245994816a84ca93be8293147b7ed1f0742859c
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2024-07-04 16:46:30 +0200

    Renamed variables for clarity

--- tris.cpp
+++ tris.cpp
@@ -6,43 +6,43 @@ using std::cout, std::cin, std::string, std::stoi;
 #define BLUE_BOLD "\033[34;1m"
 #define RESET "\033[0m"
 
-const int M = 3;
-const int N = 3;
-const int TOTAL_CELLS = M * N;
-char matrix[M][N];
+const int ROWS = 3;
+const int COLUMNS = 3;
+const int TOTAL_CELLS = ROWS * COLUMNS;
+char board[ROWS][COLUMNS];
 const string COMPUTER_NAME = "Computer";
 int matches;
 
 struct Player
 {
-    string Name;
-    char Move;
-    int Win;
+    string name;
+    char move;
+    int win;
 };
 
-Player p1{.Move = 'X'};
-Player p2{.Move = 'O'};
+Player p1{.move = 'X'};
+Player p2{.move = 'O'};
 
 int readInt();
-bool startGame(char dis);
+bool startGame(char gameMode);
 void namesPlayers();
-void printMatrix();
+void printBoard();
 void playerMove(Player player);
-void placeMarker(Player & player, int pos);
+void placeMarker(Player & player, int position);
 bool computerMove();
 bool checkWin(Player & player);
-bool checkTie(int moves);
-void resetMatrix();
+bool checkTie(int moveCount);
+void resetBoard();
 void showWins();
 bool playAgain();
 
 int main()
 {
-    int option;
-    char dis;
+    int menuChoice;
+    char gameMode;
 
-    p1.Win = 0;
-    p2.Win = 0;
+    p1.win = 0;
+    p2.win = 0;
     matches = 0;
 
     while (true)
@@ -55,27 +55,27 @@ int main()
         cout << "2. Player vs Computer\n";
         cout << "3. Exit\n";
         cout << "--> ";
-        option = readInt();
+        menuChoice = readInt();
 
-        if (option == 3)
+        if (menuChoice == 3)
         {
             cout << "Exiting program...\n";
             break;
         }
         
-        switch (option)
+        switch (menuChoice)
         {
             case 1:
-                dis = 'p';
-                p2.Name = "";
-                resetMatrix();
+                gameMode = 'p';
+                p2.name = "";
+                resetBoard();
                 namesPlayers();
                 break;
 
             case 2:
-                dis = 'c';
-                p2.Name = COMPUTER_NAME;
-                resetMatrix();
+                gameMode = 'c';
+                p2.name = COMPUTER_NAME;
+                resetBoard();
                 namesPlayers();
                 break;
 
@@ -83,9 +83,9 @@ int main()
                 continue;
         }
 
-        while (startGame(dis))
+        while (startGame(gameMode))
         {
-            resetMatrix();
+            resetBoard();
         }
     }
 
@@ -94,15 +94,15 @@ int main()
 
 int readInt()
 {
-    string input;
-    int number;
+    string inputString;
+    int inputNumber;
 
-    cin >> input;
+    cin >> inputString;
 
     try
     {
-        number = stoi(input);
-        return number;
+        inputNumber = stoi(inputString);
+        return inputNumber;
     }
     catch (...)
     {
@@ -111,15 +111,15 @@ int readInt()
 
 }
 
-bool startGame(char dis)
+bool startGame(char gameMode)
 {
-    int moves = 0;
+    int moveCount = 0;
 
     do
     {
-        printMatrix();
+        printBoard();
         showWins();
-        moves++;
+        moveCount++;
 
         if (matches % 2 == 0)
         {
@@ -127,7 +127,7 @@ bool startGame(char dis)
             if (checkWin(p1))
                 break;
         }
-        else if (dis == 'p' && matches % 2 == 1)
+        else if (gameMode == 'p' && matches % 2 == 1)
         {
             playerMove(p2);
             if (checkWin(p2))
@@ -140,12 +140,12 @@ bool startGame(char dis)
                 break;
         }
 
-        if (checkTie(moves))
+        if (checkTie(moveCount))
             break;
 
-        printMatrix();
+        printBoard();
         showWins();
-        moves++;
+        moveCount++;
 
         if (matches % 2 == 1)
         {
@@ -153,7 +153,7 @@ bool startGame(char dis)
             if (checkWin(p1))
                 break;
         }
-        else if (dis == 'p' && matches % 2 == 0)
+        else if (gameMode == 'p' && matches % 2 == 0)
         {
             playerMove(p2);
             if (checkWin(p2))
@@ -177,12 +177,12 @@ void namesPlayers()
 {
     system("clear");
     
-    if (p2.Name == COMPUTER_NAME)
+    if (p2.name == COMPUTER_NAME)
     {
         cout << "---------TRIS---------\n";
         cout << "Insert your name:\n";
         cout << "--> ";
-        cin >> p1.Name;
+        cin >> p1.name;
     }
 
     else
@@ -190,39 +190,39 @@ void namesPlayers()
         cout << "---------TRIS---------\n";
         cout << "Insert FIRST player's name:\n";
         cout << "--> ";
-        cin >> p1.Name;
+        cin >> p1.name;
 
         cout << "\nInsert SECOND player's name:\n";
         cout << "--> ";
-        cin >> p2.Name;
+        cin >> p2.name;
     }
 }
 
-void printMatrix()
+void printBoard()
 {
     system("clear");
 
     cout << "---------TRIS---------\n\n";
 
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < ROWS; i++)
     {
         cout << "     ";
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < COLUMNS; j++)
         {
-            if (matrix[i][j] == 'X')
-                cout << RED_BOLD << " " << matrix[i][j] << " " << RESET;
-            else if (matrix[i][j] == 'O')
-                cout << BLUE_BOLD << " " << matrix[i][j] << " " << RESET;
+            if (board[i][j] == 'X')
+                cout << RED_BOLD << " " << board[i][j] << " " << RESET;
+            else if (board[i][j] == 'O')
+                cout << BLUE_BOLD << " " << board[i][j] << " " << RESET;
             else
-                cout << " " << matrix[i][j] << " ";
+                cout << " " << board[i][j] << " ";
 
-            if (j < N - 1)
+            if (j < COLUMNS - 1)
                 cout << "|";
         }
 
         cout << "\n";
 
-        if (i < M - 1)
+        if (i < ROWS - 1)
         {
             cout << "     " << "-----------\n";
         }
@@ -233,32 +233,32 @@ void printMatrix()
 
 void playerMove(Player player)
 {
-    int pos;
+    int position;
 
-    cout << player.Name << "'s turn\n";
-    cout << "Choose where to place your \"" << player.Move << "\": ";
-    pos = readInt();
+    cout << player.name << "'s turn\n";
+    cout << "Choose where to place your \"" << player.move << "\": ";
+    position = readInt();
 
-    placeMarker(player, pos);
+    placeMarker(player, position);
 }
 
-void placeMarker(Player & player, int pos)
+void placeMarker(Player & player, int position)
 {
-    int row = (pos - 1) / N;
-    int col = (pos - 1) % N;
+    int rowIndex = (position - 1) / COLUMNS;
+    int colIndex = (position - 1) % COLUMNS;
 
-    if (pos >= 1 && pos <= TOTAL_CELLS && matrix[row][col] != 'X' && matrix[row][col] != 'O')
+    if (position >= 1 && position <= TOTAL_CELLS && board[rowIndex][colIndex] != 'X' && board[rowIndex][colIndex] != 'O')
     {
-        matrix[row][col] = player.Move;
+        board[rowIndex][colIndex] = player.move;
     }
 
     else
     {
-        printMatrix();
+        printBoard();
         
-        if (pos == 0)
+        if (position == 0)
             cout << "Invalid input.\nChoose a position between 1 and 9.\n";
-        else if (pos < 1 || pos > 9)
+        else if (position < 1 || position > 9)
             cout << "Not a valid position.\nChoose a position between 1 and 9.\n";
         else
             cout << "Position alredy occupied.\nChoose another position.\n";
@@ -270,93 +270,93 @@ void placeMarker(Player & player, int pos)
 
 bool computerMove()
 {
-    for (int i = 0; i < M; i++) 
+    for (int i = 0; i < ROWS; i++) 
     {
-        for (int j = 0; j < N; j++) 
+        for (int j = 0; j < COLUMNS; j++) 
         {
-            if (matrix[i][j] != 'X' && matrix[i][j] != 'O') 
+            if (board[i][j] != 'X' && board[i][j] != 'O') 
             {
-                char original_value = matrix[i][j];
-                matrix[i][j] = p2.Move;
+                char originalValue = board[i][j];
+                board[i][j] = p2.move;
 
                 if (checkWin(p2))
                 {
-                    p2.Win--;
+                    p2.win--;
                     return true;
                 }
 
-                matrix[i][j] = original_value;
+                board[i][j] = originalValue;
             }
         }
     }
 
-    for (int i = 0; i < M; i++) 
+    for (int i = 0; i < ROWS; i++) 
     {
-        for (int j = 0; j < N; j++) 
+        for (int j = 0; j < COLUMNS; j++) 
         {
-            if (matrix[i][j] != 'X' && matrix[i][j] != 'O') 
+            if (board[i][j] != 'X' && board[i][j] != 'O') 
             {
-                char original_value = matrix[i][j];
-                matrix[i][j] = p1.Move;
+                char originalValue = board[i][j];
+                board[i][j] = p1.move;
 
                 if (checkWin(p2)) 
                 {
-                    p2.Win--;
-                    matrix[i][j] = p2.Move;
+                    p2.win--;
+                    board[i][j] = p2.move;
                     return true;
                 }
 
-                matrix[i][j] = original_value;
+                board[i][j] = originalValue;
             }
         }
     }
 
-    int pos;
+    int position;
 
     do 
     {
-        pos = rand() % TOTAL_CELLS + 1;
+        position = rand() % TOTAL_CELLS + 1;
     } 
-    while (matrix[(pos - 1) / N][(pos - 1) % N] == 'X' || matrix[(pos - 1) / N][(pos - 1) % N] == 'O');
+    while (board[(position - 1) / COLUMNS][(position - 1) % COLUMNS] == 'X' || board[(position - 1) / COLUMNS][(position - 1) % COLUMNS] == 'O');
 
-    placeMarker(p2, pos);
+    placeMarker(p2, position);
 
     return false;
 }
 
 bool checkWin(Player & player)
 {
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < ROWS; i++)
     {
-        if ((matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2]) ||
-            (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i]))
+        if ((board[i][0] == board[i][1] && board[i][0] == board[i][2]) ||
+            (board[0][i] == board[1][i] && board[0][i] == board[2][i]))
         {
-            printMatrix();
-            player.Win++;
+            printBoard();
+            player.win++;
             showWins();
-            cout << player.Name << " won!!\n";
+            cout << player.name << " won!!\n";
             return true;
         }
     }
 
-    if ((matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2]) ||
-        (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0]))
+    if ((board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
+        (board[0][2] == board[1][1] && board[0][2] == board[2][0]))
     {
-        printMatrix();
-        player.Win++;
+        printBoard();
+        player.win++;
         showWins();
-        cout << player.Name << " won!!\n";
+        cout << player.name << " won!!\n";
         return true;
     }
 
     return false;
 }
 
-bool checkTie(int moves)
+bool checkTie(int moveCount)
 {
-    if (moves == TOTAL_CELLS)
+    if (moveCount == TOTAL_CELLS)
     {
-        printMatrix();
+        printBoard();
         showWins();
         cout << "It's a tie.\n";
         return true;
@@ -365,21 +365,21 @@ bool checkTie(int moves)
     return false;
 }
 
-void resetMatrix()
+void resetBoard()
 {
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < ROWS; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < COLUMNS; j++)
         {
-            matrix[i][j] = '1' + i * N + j;
+            board[i][j] = '1' + i * COLUMNS + j;
         }
     }
 }
 
 void showWins()
 {
-    cout << p1.Name << "'s wins: " << p1.Win << "\n";
-    cout << p2.Name << "'s wins: " << p2.Win << "\n";
+    cout << p1.name << "'s wins: " << p1.win << "\n";
+    cout << p2.name << "'s wins: " << p2.win << "\n";
     cout << "----------------------\n";
 }
 
@@ -402,7 +402,7 @@ bool playAgain()
         }
         else
         {
-            printMatrix();
+            printBoard();
             showWins();
             cout << "Not a valid option.\n";
         }
